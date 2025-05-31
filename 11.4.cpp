574821eb9a7f2e8554e87a6a872efe4c6c40bbca
Carlos Gallardo <cgallardos@unjbg.edu.pe>
2025-05-31 09:42:18 -0500

    Actualiza ejercicio 11 de estructuras repetitivas: aumenta el tamaño del número usando long long

---
+++ 11.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+long long int n, i, f;
+f=1;
+cout<<"Inserte un numero entero positivo: "<<endl;
+cin>>n;
+if (n<0){
+	cout<<"El numero debe ser entero positivo";
+	}else{
+		for (i=1;i<=n;i++){
+		f=f*i;
+	}
+	cout<<"El factorial de "<<n<<" es: "<<f<<endl;	
+	}
+}
